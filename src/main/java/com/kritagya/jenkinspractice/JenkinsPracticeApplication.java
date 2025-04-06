f16c4922e2eb104552d86ea4911d3d06948c9bdf
kritagya.shakya <kritagya.shakya@f1soft.com>
2025-04-06 17:26:28 +0545

    Added: test and print hello world

--- src/main/java/com/kritagya/jenkinspractice/JenkinsPracticeApplication.java
+++ src/main/java/com/kritagya/jenkinspractice/JenkinsPracticeApplication.java
@@ -6,8 +6,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JenkinsPracticeApplication {
 
+    private static final String MESSAGE = "Hello World!";
+
     public static void main(String[] args) {
         SpringApplication.run(JenkinsPracticeApplication.class, args);
+        System.out.println(MESSAGE);
+    }
+
+    public String getMessage() {
+        return MESSAGE;
     }
 
 }
--- src/test/java/com/kritagya/jenkinspractice/JenkinsPracticeApplicationTests.java
+++ src/test/java/com/kritagya/jenkinspractice/JenkinsPracticeApplicationTests.java
@@ -2,12 +2,26 @@ package com.kritagya.jenkinspractice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class JenkinsPracticeApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void testConstructor() {
+        JenkinsPracticeApplication application = new JenkinsPracticeApplication();
+        JenkinsPracticeApplication application2 = new JenkinsPracticeApplication();
+        assertEquals(application.getMessage(), application2.getMessage());
+    }
+
+    @Test
+    public void testMessage() {
+        JenkinsPracticeApplication application = new JenkinsPracticeApplication();
+        assertEquals("Hello World!",application.getMessage());
     }
 
 }
